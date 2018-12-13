7b2bcd8679bd8c5ea5d8315b42bc1c75f9c2b72b
squee <squeemishly@gmail.com>
2018-12-13 16:27:57 -0700

    create users

---
+++ db/migrate/20181213210005_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :first_name, :null => false
+      t.string :last_name, :null => false
+      t.string :username, :unique => true, :null => false
+      t.string :email, :unique => true, :null => false
+      t.string :password, :null => false
+
+      t.timestamps
+    end
+  end
+end
