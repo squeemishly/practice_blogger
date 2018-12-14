ade2d59611953470984411f5bcf244aa103e9783
squee <squeemishly@gmail.com>
2018-12-14 12:58:57 -0700

    write tests for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
+  has_many :articles
+
   validates :username, presence: true, uniqueness: true
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :password, presence: true
   validates :email, presence: true, uniqueness: true
 
   def authenticate(username, password)
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "attributes" do
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password) }
+    it { should validate_presence_of(:username) }
+  end
+
+  context "relationships" do
+    it { should have_many(:articles) }
+  end
+
+  context "methods" do
+    context ".authenticate" do
+      attr_reader :user
+
+      before(:each) do
+        @user = User.create(
+          first_name: :fake_first_name,
+          last_name: :fake_last_name,
+          email: :fake_email,
+          password: :fake_pass,
+          username: :fake_username
+        )
+      end
+
+      it "returns the user when username and password are correct" do
+        expect(user.authenticate(user.username, user.password)).to eq user
+      end
+
+      it "returns nil with an incorrect password" do
+        expect(user.authenticate(user.username, :diff_pass)).to be_nil
+      end
+
+      it "returns nil with an incorrect username" do
+        expect(user.authenticate(:diff_user, user.password)).to be_nil
+      end
+    end
+  end
 end
