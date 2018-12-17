0c699f100098665fbd4d7f7412fe95b5f2496495
squee <squeemishly@gmail.com>
2018-12-17 16:33:06 -0700

    move comment logic to model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,11 @@ class Comment < ApplicationRecord
   belongs_to :user
 
   validates :body, presence: true
+
+  def self.create_comment(comment_params, article, user)
+    comment = new(comment_params)
+    comment.article = article
+    comment.user = user
+    comment
+  end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -9,4 +9,31 @@ RSpec.describe Comment, type: :model do
     it { should belong_to(:user) }
     it { should belong_to(:article) }
   end
+
+  context "methods" do
+    context ".create_comment" do
+      it "should return a new comment" do
+        user = User.create(
+          first_name: "fake_first",
+          last_name: "fake_last",
+          email: "fake_email",
+          password: "fakeyfakefake",
+          username: "fake_user"
+        )
+        article = Article.create(
+          user: user,
+          title: "fake_title",
+          body: "fake_body"
+        )
+        params = {body: "fake"}
+
+        comment = Comment.create_comment(params, article, user)
+
+        expect(comment).to be_a Comment
+        expect(comment.body).to eq "fake"
+        expect(comment.user).to eq user
+        expect(comment.article).to eq article
+      end
+    end
+  end
 end
