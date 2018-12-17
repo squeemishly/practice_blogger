08b112a351081cd434a04f8db58a0ad6942e6e94
squee <squeemishly@gmail.com>
2018-12-17 12:26:23 -0700

    create seed file

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'coffee-rails', '~> 4.2'
 gem 'jbuilder', '~> 2.5'
 gem 'bootsnap', '>= 1.1.0', require: false
 gem 'bcrypt'
+gem 'faker'
 
 group :development, :test do
   gem 'rspec-rails'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,71 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+users = []
+articles = []
+
+10.times do
+  user = User.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    username: Faker::Cat.unique.name,
+    email: Faker::Internet.unique.email,
+    password: Faker::Pokemon.name
+  )
+
+  users << user
+
+  puts "#{user.username} has been created with password #{user.password}!"
+end
+
+20.times do
+  user = users.sample
+
+  article = Article.new(
+    title: Faker::FamousLastWords.unique.last_words,
+    body: Faker::ChuckNorris.unique.fact,
+    user: user
+  )
+
+  articles << article
+
+  puts "#{article.title} was created by #{user.username}!"
+end
+
+20.times do
+  user = users.sample
+
+  article = Article.new(
+    title: Faker::StarWars.unique.quote,
+    body: Faker::ChuckNorris.unique.fact,
+    user: user
+  )
+
+  articles << article
+
+  puts "#{article.title} was created by #{user.username}!"
+end
+
+20.times do
+  user = users.sample
+
+  article = Article.new(
+    title: Faker::WorldOfWarcraft.unique.quote,
+    body: Faker::PrincessBride.unique.quote,
+    user: user
+  )
+
+  articles << article
+
+  puts "#{article.title} was created by #{user.username}!"
+end
+
+50.times do
+  user = users.sample
+  article = articles.sample
+
+  comment = Comment.new(
+    body: Faker::Hacker.unique.say_something_smart,
+    user: user,
+    article: article
+  )
+
+  puts "#{user.username} said something smart about #{article.title}"
+end
