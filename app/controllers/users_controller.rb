57b217a562180ac9f9b627d64d93507271f880d4
squee <squeemishly@gmail.com>
2019-01-24 16:21:13 -0700

    add avatar to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,6 +34,6 @@ class UsersController < ApplicationController
 
   private
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :username, :password)
+      params.require(:user).permit(:first_name, :last_name, :email, :username, :password, :avatar)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_one_attached :avatar
   has_many :articles, dependent: :destroy
 
   validates :username, presence: true, uniqueness: true
